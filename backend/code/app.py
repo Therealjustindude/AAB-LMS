3fed414da65b571e1eeeea06aaa942b59cac4491
Aneesh Kodali <aneesh.kodali@gmail.com>
2021-02-03 15:22:46 -0500

    Create basic flask app (#2)

---
+++ backend/code/app.py
@@ -0,0 +1,19 @@
+#### IMPORTS
+
+## python imports
+from flask import Flask
+
+## project imports
+
+
+#### APP CONFIG
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return "Hello World"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
