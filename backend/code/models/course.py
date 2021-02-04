41af3593b05eafc09dcf395d1fbe8f15626a916e
Aneesh Kodali <aneesh.kodali@gmail.com>
2021-02-03 22:33:21 -0500

    Add json method to CourseModel (#2)

--- backend/code/models/course.py
+++ backend/code/models/course.py
@@ -1,4 +1,8 @@
+# python imports
+from sqlalchemy import Column, String
+
 # project imports
+from db import db
 from models.base import BaseModel
 
 class CourseModel(BaseModel, db.Model):
@@ -6,4 +10,16 @@ class CourseModel(BaseModel, db.Model):
     Course
     '''
 
-    course = Column(String, nullable=False)
+    __tablename__ = 'courses' 
+
+    course = Column(
+                String, 
+                nullable=False
+    )
+
+    # method to return dictionary version
+    def json(self):
+        return {
+                'id': course.id, 
+                'course': course.course
+            }
