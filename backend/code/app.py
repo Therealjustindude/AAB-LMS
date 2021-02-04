f254237e6486a0b7046aab4e0417a5f79bf58cf6
Aneesh Kodali <aneesh.kodali@gmail.com>
2021-02-03 22:37:24 -0500

    Connect app to sqlite db (#2)

--- backend/code/app.py
+++ backend/code/app.py
@@ -5,14 +5,28 @@ from flask import Flask
 from flask_restful import Api
 
 ## project imports
+from db import db
+from models.course import CourseModel
 from resources.course import Courses
 
 #### APP CONFIG
 
 # initialize
 app = Flask(__name__)
+# connect db to app
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 # connect flask_restful to app
 api = Api(app)
+
+# create db if DNE before first request is run
+# SQLAlchemy can only create tables it sees (it sees tables because we import the resource which imports the model)
+# we can also import models directly and db will create
+# ex. import Courses resource <= Courses imports CourseModel <= CourseModel has tablename
+@app.before_first_request
+def create_tables():
+    db.create_all()
 
 
 #### ENDPOINT CONFIG
@@ -26,4 +40,5 @@ api.add_resource(Courses, '/backend/courses')
 
 # RUN APP
 if __name__ == "__main__":
+    db.init_app(app)
     app.run(debug=True)
