7489a18d33af7d34bbff9e9c21580674fc898aaa
Aneesh Kodali <aneesh.kodali@gmail.com>
2021-02-03 20:51:42 -0500

    Add flask-restful to app (#2)

--- backend/code/app.py
+++ backend/code/app.py
@@ -2,18 +2,25 @@
 
 ## python imports
 from flask import Flask
+from flask_restful import Api
 
 ## project imports
 
 
 #### APP CONFIG
-app = Flask(__name__)
 
+# initialize
+app = Flask(__name__)
+# connect flask_restful to app
+api = Api(app)
+
+
+#### ENDPOINT CONFIG
 
 @app.route('/')
 def index():
     return "Hello World"
 
-
+# RUN APP
 if __name__ == "__main__":
     app.run(debug=True)
