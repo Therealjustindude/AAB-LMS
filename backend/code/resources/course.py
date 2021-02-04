85fa7fb62f76b4dc54774cef5ac7d13763ee85d4
Aneesh Kodali <aneesh.kodali@gmail.com>
2021-02-03 22:34:13 -0500

    Edit Courses resource get method to query courses table (#2)

--- backend/code/resources/course.py
+++ backend/code/resources/course.py
@@ -1,7 +1,11 @@
+# python imports
 from flask_restful import Resource
+
+# project imports
+from models.course import CourseModel
 
 
 class Courses(Resource):
 
     def get(self):
-        return {'courses': ['java', 'python', 'c++']}
+        return {'courses': [course.json() for course in CourseModel.query.all()]}
