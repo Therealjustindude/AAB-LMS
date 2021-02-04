b563f2a2c99404b308cc63d1e33485576709ee91
therealjustindude <therealjustindude@gmail.com>
2021-02-04 15:12:18 -0600

    everything profile renamed to user, / route now linked to Courses and Courses tabbed changed to UserCourses

--- Frontend/src/Routes.js
+++ Frontend/src/Routes.js
@@ -1,18 +1,18 @@
 import React from "react";
 import {withRouter, Route, Switch} from "react-router-dom";
 
-import Home from "./Components/Home";
 import SingleCourse from "./Components/SingleCourse";
 import Courses from "./Components/Courses";
-import Profile from "./Components/Profile";
+import User from "./Components/User";
+import UserCourses from "./Components/UserCourses";
 
 function Routes() {
   return (
     <Switch>
-      <Route path="/Courses/:name" component={SingleCourse}/>
-      <Route path="/Courses" component={Courses}/>
-      <Route path="/Profile" component={Profile}/>
-      <Route path="/" component={Home}/>
+      <Route exact path="/" component={Courses}/>
+      <Route exact path="/Courses/:id" render={(routerProps) => <SingleCourse history={routerProps.history} />}/>
+      <Route exact path="/user/:id" render={(routerProps) => <User history={routerProps.history} />}/>
+      <Route exact path="/user/:id/courses" render={(routerProps) => <UserCourses history={routerProps.history} />}/>
     </Switch>
   )
 }
