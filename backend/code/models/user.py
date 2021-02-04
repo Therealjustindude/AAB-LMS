6f37d9999372b45fad33884ec15f38c2448bbf1b
Aneesh Kodali <aneesh.kodali@gmail.com>
2021-02-04 06:22:19 -0500

    Create UserModel (#2)

---
+++ backend/code/models/user.py
@@ -0,0 +1,40 @@
+# python imports
+from sqlalchemy import Column, String
+
+# project imports
+from db import db
+from models.base import BaseMixin
+
+class UsereModel(BaseMixin, db.Model):
+    '''
+    User
+    '''
+
+    __tablename__ = 'users' 
+
+    first_name = Column(
+                String, 
+                nullable=False
+    )
+    last_name = Column(
+                String, 
+                nullable=False
+    )
+    email = Column(
+                String, 
+                nullable=False,
+                unique=True
+    )
+    password = Column(
+                String, 
+                nullable=False
+    )
+
+    # method to return dictionary version
+    def json(self):
+        return {
+                'id': self.id, 
+                'first name': self.first_name,
+                'last_name': self.last_name,
+                'email': self.email
+            }
