68fdf0c77bf9b720b502c0a25f94234b191361de
therealjustindude <therealjustindude@gmail.com>
2021-02-04 15:11:16 -0600

    redux store created and provider wrapped around apps main container

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,16 +1,26 @@
+// imports for redux
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './store/reducers/indexReducer'
+
+
 import './App.css';
 import Container from "react-bootstrap/Container";
-
-// import Home from "./Components/Home"
 import Routes from "./Routes"
 import NavigationBar from "./Components/NavigationBar"
 
+// create redux store
+const store = createStore(rootReducer, applyMiddleware(thunk))
+
 function App() {
   return (
-    <Container fluid className="App">
-      <NavigationBar/>
-      <Routes/>
-    </Container>
+    <Provider store={store}>
+      <Container fluid className="App">
+        <NavigationBar/>
+        <Routes/>
+      </Container>
+    </Provider>
   );
 }
 
