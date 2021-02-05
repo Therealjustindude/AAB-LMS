1cccc4afb01cfb10c2a74e672ec71a125f7ff807
Aneesh Kodali <aneesh.kodali@gmail.com>
2021-02-05 16:13:57 -0500

    Fix error in json method (#2)

--- backend/code/models/user.py
+++ backend/code/models/user.py
@@ -44,7 +44,7 @@ class UserModel(BaseMixin, db.Model):
     def json(self):
         return {
                 'id': self.id, 
-                'first name': self.first_name,
+                'first_name': self.first_name,
                 'last_name': self.last_name,
                 'email': self.email
             }
