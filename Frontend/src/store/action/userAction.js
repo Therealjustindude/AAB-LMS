4adf3b4c1850792a106899f53165b2d45a06e059
vlee4 <vicglee1234007@gmail.com>
2021-02-04 18:44:50 -0600

    Add back in backticks to BaseUrl in fetch call

--- Frontend/src/store/action/userAction.js
+++ Frontend/src/store/action/userAction.js
@@ -2,7 +2,7 @@ const BASE_URL = "http://127.0.0.1:5000/backend"
 
 export const getUser = () => {
     return (() => {
-        fetch(${BASE_URL}/users,{
+        fetch(`${BASE_URL}/users`,{
             method: "GET",
             headers: {
                 "Content-Type": "application/json",
