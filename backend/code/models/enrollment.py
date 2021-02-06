caca795c79029ad2d25b2395509e98e8f09f5595
Aneesh Kodali <aneesh.kodali@gmail.com>
2021-02-06 10:54:44 -0500

    Remove extra directories (#2)

--- backend/code/models/enrollment.py
+++
@@ -1,8 +0,0 @@
-# project imports
-from db import db
-
-enrollments = db.Table(
-    'enrollments',
-    db.Column('user_id', db.String, db.ForeignKey('users.id')),
-    db.Column('course_id', db.String, db.ForeignKey('courses.id'))
-)
